efdaa9af78993ee5053a079fe93db3d8c5d0f2e3
Peng_Lx <L834787411@outlook.com>
2023-08-15 10:46:53 +0800

    Update AutoRegister.kt

--- platform/platform-starkkit/src/main/kotlin/taboolib/platform/autoregister/AutoRegister.kt
+++ platform/platform-starkkit/src/main/kotlin/taboolib/platform/autoregister/AutoRegister.kt
@@ -1,4 +1,8 @@
 package taboolib.platform.autoregister
 
+import java.lang.annotation.Inherited
+
+@Target(AnnotationTarget.CLASS)
+@Inherited
 @Retention(AnnotationRetention.RUNTIME)
 annotation class AutoRegister(val value: RegisterType)
